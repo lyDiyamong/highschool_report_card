8cb03651a01a68311a774d4621bd259fa8fb3f16
lyDiyamong <diyamong26@gmail.com>
2025-02-25 15:20:43 +0700

    add: clickhouse + axios configuration

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 import './assets/main.css'
 import router from './routes'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+const pinia = createPinia()
 app.use(Antd)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
